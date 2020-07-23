02fdfa9cef1681107d96b98c9247d67a8c0e087e
Ammar Abdelwahed <aabdelwahed3@gatech.edu>
2020-07-23 13:01:39 -0500

    Finsihed logic part now I have to figure out how to write back to file

--- IndividualProject/fileprocessor/src/edu/gatech/seclass/fileprocessor/FileProcessor.java
+++ IndividualProject/fileprocessor/src/edu/gatech/seclass/fileprocessor/FileProcessor.java
@@ -86,32 +86,110 @@ public class FileProcessor implements FileProcessorInterface {
   public void process() throws ProcessingException {
 
     String fileContent = getFileContent(filepath);
+
+    //Convert fileContent Into Array
     String[] arrayFile = fileContent.split("\\r?\\n");
-        ArrayList <String> a = new ArrayList<String>();
-        for (String line : arrayFile){
-            a.add(line);
-        }
-        if(list) {
-          ArrayList<String> tmpArray = new ArrayList<String>();
-          int counter = 1;
-          for (String line : a) {
-              line = counter + " " + line;
-              counter++;
-              tmpArray.add(line);
+    ArrayList <String> a = new ArrayList<String>();
+    for (String line : arrayFile){
+        a.add(line);
+    }
+
+    //Sorting Function
+    if(sort){
+      ArrayList<String> tmpArray = new ArrayList<String>();
+      ArrayList<ArrayList> fullArray = new ArrayList<ArrayList>();
+      for (String line : a) {
+          ArrayList<String> isDifferent = new ArrayList<String>();
+          String onlySpecialChar = line.replaceAll("[^a-zA-z0-9]", "");
+          isDifferent.add(line);
+          isDifferent.add(onlySpecialChar);
+          fullArray.add(isDifferent);
+          if(!onlySpecialChar.equals("")){
+              tmpArray.add(onlySpecialChar);
           }
-          a = tmpArray;
       }
+      Collections.sort(tmpArray);
+      ArrayList<String> thisIsIt = new ArrayList<String>();
+      for(String item : tmpArray){
+          for(int i = 0; i < fullArray.size(); i++){
+              if(item.equals(fullArray.get(i).get(1))){
+                  thisIsIt.add(fullArray.get(i).get(0).toString());
+              }
+
+          }
+      }
+      a = thisIsIt;
+    }
+
+    //Keep function
+    if(!keep.equals("")){
+        ArrayList<String> tmpArray = new ArrayList<String>();
+        for(String line : a) {
+            boolean isPresent = line.indexOf(keep) != -1 ? true : false;
+            if(isPresent) {
+                tmpArray.add(line);
+            }
+        }
+        a = tmpArray;
+    }
+
+    //Remove function
+    if(!remove.equals("")){
+        ArrayList<String> tmpArray = new ArrayList<String>();
+        for(String line : a) {
+            boolean isPresent = line.indexOf(remove) != -1 ? true : false;
+            if(!isPresent) {
+                tmpArray.add(line);
+            }
+        }
+        a = tmpArray;
+    }
+
+    //Trim function
+    if(trim <= 0){
+        // System.out.print("You cant do that!!");
+    } else {
+        // if(!trim) trim = 1;
+        ArrayList<String> tmpArray = new ArrayList<String>();
+        for(String line : a){
+            if(trim > (line.length())) {
+                String split = line.substring(0, line.length());
+                tmpArray.add(split);
+            } else {
+                String split = line.substring(0, trim);
+                tmpArray.add(split);
+            }
+        }
+        a = tmpArray;
+    }
+
+    //List Function
+    if(list) {
+        ArrayList<String> tmpArray = new ArrayList<String>();
+        int counter = 1;
+        for (String line : a) {
+            line = counter + " " + line;
+            counter++;
+            tmpArray.add(line);
+        }
+        a = tmpArray;
+    }
 
 
-      String output = "";
-      for(String line : a){
-          output = output + line + System.lineSeparator();
-      }
-      try{
+    //Convert Array to String
+    String output = "";
+    for(String line : a){
+        output = output + line + System.lineSeparator();
+    }
+
+
+
+
+    try {
         writeOutput(output);
-      } catch (IOException e) {
+    } catch (IOException e) {
         e.printStackTrace();
-      }
+    }
 
   }
   
