1a914a7b5be12a8a5cf5d2b2dcefda2f0283e616
Ammar Abdelwahed <aabdelwahed3@gatech.edu>
2020-07-26 15:36:58 -0500

    D3 Doing fun stuff

--- IndividualProject/fileprocessor/src/edu/gatech/seclass/fileprocessor/FileProcessor.java
+++ IndividualProject/fileprocessor/src/edu/gatech/seclass/fileprocessor/FileProcessor.java
@@ -95,7 +95,7 @@ public class FileProcessor implements FileProcessorInterface {
 
   @Override
   public void process() throws ProcessingException {
-    if(filepath == null || filepath.equals("") || filepath.equals("-1")){
+    if(filepath == null || filepath.equals("") || filepath.equals("-1") || filepath.isEmpty()){
         throw new ProcessingException("No filename provided");
     }
 //    if(!keep.equals("") && !remove.equals("")){
