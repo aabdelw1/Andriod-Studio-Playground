c4a7dc7254b45c175ee0b6be64d9035756addb00
Ammar Abdelwahed <aabdelwahed3@gatech.edu>
2020-07-26 21:12:20 -0500

    D3 Doing fun stuff

--- IndividualProject/fileprocessor/src/edu/gatech/seclass/fileprocessor/FileProcessor.java
+++ IndividualProject/fileprocessor/src/edu/gatech/seclass/fileprocessor/FileProcessor.java
@@ -107,9 +107,10 @@ public class FileProcessor implements FileProcessorInterface {
     } else if(splitFileName.length == 1){
         if(splitFileName[0].equals("")){
             throw new ProcessingException("Invalid filename");
-        } else if(!splitFileName[0].equals("-1")) {
-            throw new ProcessingException("No filename provided" + fileName.equals(-1) + filepath.equals(-1));
         }
+//        else if(!splitFileName[0].equals("-1")) {
+//            throw new ProcessingException("No filename provided");
+//        }
     } else if(splitFileName.length == 2 && !splitFileName[1].equals("tmp")){
         throw new ProcessingException("Cannot read file: No_such_file.txt");
     }
